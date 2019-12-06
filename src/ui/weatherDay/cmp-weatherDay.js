93841967bc8d3cc4062e1ecccbcced94ece7c4fa
Int_21h <yuskovss@gmail.com>
2019-12-06 10:20:31 +0400

    feat: add current time to cmp-weatherDay

--- src/ui/weatherDay/cmp-weatherDay.js
+++ src/ui/weatherDay/cmp-weatherDay.js
@@ -2,11 +2,31 @@
 import Component from '../component';
 import { getState } from '../../store/state/state';
 import translite from '../../utils/translite';
-import { celsiusToFaringate /* , addSero */ } from '../../utils/utils';
+import { celsiusToFaringate, addSero } from '../../utils/utils';
 
 export default class WeatherDay extends Component {
+  constructor(className) {
+    super(className);
+    this.timerID = setInterval(() => this.getCurrentDateTime(), 100000);
+    this.currentDateTime = '';
+    this.getCurrentDateTime();
+  }
+
+  getCurrentDateTime() {
+    const { controlPanel, weather } = getState();
+    const date = new Date();
+    date.setUTCSeconds(date.getUTCSeconds() + weather.timezone);
+    const dayName = translite(controlPanel.lang, 'shortDayName', date.getUTCDay());
+    const day = date.getUTCDate();
+    const month = translite(controlPanel.lang, 'fullMonthName', date.getUTCMonth());
+    const hour = addSero(date.getUTCHours());
+    const min = addSero(date.getUTCMinutes());
+    this.currentDateTime = `${dayName} ${day} ${month}  ${hour}:${min}`;
+    this.render();
+  }
+
+
   render() {
-    console.log(getState());
     const { weather, city } = getState().weather;
     const { lang, isCelsius } = getState().controlPanel;
     if (city) {
@@ -14,14 +34,14 @@ export default class WeatherDay extends Component {
       const feels = isCelsius ? weather.feels : celsiusToFaringate(weather.feels);
       const html = `
       <div class="weather-day__city">${city.name}, ${city.country}</div>
-      <div class="weather-day__date-time">{this.state.currentDateTime}</div>
+      <div class="weather-day__date-time">${this.currentDateTime}</div>
       <div class="weather-day__wrapper">
         <div class="weather-day__temp">
           <div class="weather-day__temp-value">${temp}</div>
           <div class="weather-day__temp-deg">&deg;</div>
         </div>
         <div class="weather-day__img">
-          <img src="" alt=""></img>
+          <img src="./images/${weather.icon}.png" alt=""></img>
         </div>
         <ul class="weather-day__more">
           <li class="weather-day__more-item">${translite(lang, 'weather', weather.id)}</li>
