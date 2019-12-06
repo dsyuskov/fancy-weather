2b1c37af4665ad336373d1f4865cb6b1355fb8d3
Int_21h <yuskovss@gmail.com>
2019-12-06 14:30:10 +0400

    fix: change className to class

--- src/ui/weatherForecast/cmp-weatherForecast.js
+++ src/ui/weatherForecast/cmp-weatherForecast.js
@@ -13,10 +13,10 @@ export default class WeatherForecast extends Component {
     if (forecast !== '') {
       forecast.forEach((forecastDay) => {
         const temp = isCelsius ? forecastDay.temp : celsiusToFaringate(forecastDay.temp);
-        html += `<div className="weather-forecast__day">
-        <div className="weather-forecast__weekday">${translite(lang, 'fullDayName', forecastDay.weekDay)}</div>
-        <div className="weather-forecast__temp">${temp}&deg;</div>
-        <div className="weather-forecast__img">
+        html += `<div class="weather-forecast__day">
+        <div class="weather-forecast__weekday">${translite(lang, 'fullDayName', forecastDay.weekDay)}</div>
+        <div class="weather-forecast__temp">${temp}&deg;</div>
+        <div class="weather-forecast__img">
           <img src="./images/${forecastDay.icon}.png" alt="">
         </div>
       </div>`;
